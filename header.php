cd5cf5b21bf94f3ab59755e787654ee94fed04c3
Christian Colglazier <aquamorphcomputing@gmail.com>
2014-10-04 13:38:59 -0400

    Improved mobile menu.

--- header.php
+++ header.php
@@ -1,4 +1,20 @@
+<script>
+function toggleVisibility(id) {
+       var e = document.getElementById(id);
+       if(e.style.display == 'block')
+          e.style.display = 'none';
+       else
+          e.style.display = 'block';
+    }
+</script>
 <div class="header">
-<?php wp_nav_menu( array( 'theme_location' => 'menu-main' ) ); ?>
+	<div id="mobile">
+    	<div id="box" onClick="toggleVisibility('menu')"></div>
+    	<h1><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1>
+        
+    </div>
+	<div id="menu">
+	<?php wp_nav_menu( array( 'theme_location' => 'menu-main' ) ); ?>
+    </div>
 </div>
 
